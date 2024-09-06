cb222bde621beb7a95c66d205b0661aa204be44c
Leonardo Dias <leonardo.bispo1222@gmail.com>
2024-09-06 19:54:46 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,64 @@
+let dados = [
+    {titulo : "Poze Anos 80",
+    descricao : `MC Poze do Rodo A Ascensão de um Ícone do Funk
+                MC Poze do Rodo, cujo nome verdadeiro é Marlon Brandon Coelho Couto Silva, é um dos maiores nomes do funk carioca da atualidade. 
+                Nascido e criado na comunidade do Rodo, em Santa Cruz, Zona Oeste do Rio de Janeiro, Poze conquistou o Brasil com sua música autêntica e 
+                letras que retratam a realidade das favelas 
+                De onde veio o sucesso?Origens humildes: Poze teve uma infância marcada pelas dificuldades da vida na periferia e, como muitos jovens de sua comunidade, 
+                envolveu-se com o crime.A música como escape: A música se tornou uma forma de expressar suas experiências e sentimentos, e logo ele começou a ganhar destaque nas redes sociais.
+                Homenagem que viralizou: O hit "Homenagem Pra Tropa do Rodo" foi o ponto de virada em sua carreira, impulsionando-o para a fama nacional.
+                Verdade nas letras: As letras de Poze são conhecidas por sua sinceridade e por abordar temas como a vida na favela, a violência, o amor e a superação.
+                Por que Poze faz tanto sucesso`,
+    link : "https://www.youtube.com/watch?v=WZIGwN-5Ioo",
+    tags : "MC Pose mc pose pose do rodo rodo Marlon Brandon Coelho Couto Silva posedorodo",
+},
+        
+    {titulo : "MC Gorila",
+    descricao : `Paulo Fernandes (Rio de Janeiro, 30/04/1978), mais conhecido pelo nome artístico "MC GORILA"; 
+                Nos meados da década de 90, surgiam no cenário do Funk Carioca, a Dupla de mc´s "GORILA E PRETO", na própria comunidade
+                onde foi nascido e criado ROCINHA.`,
+    link : "https://www.youtube.com/watch?v=YlyBiUJKN8c",
+    tags : "Gorila MC Gorila mcgorila paulo fernandes",
+},
+
+    {titulo : "MC VV",
+    descricao : `(Então, lindos mergulhadores sexys)
+                     Cês tão pronto, filha da puta?
+                     Pra partir de barco pra uma suruba
+                     Em alto mar com o capitão
+                     Que televisão? É a Fenda da Sunga</p>
+                     <p class="descricao-meta">
+                        Eu não ouvi direito, filhas da puta
+                        (Estamos, capitão)
+                         Então fecha o nariz e pula, ooh
+
+                        Vive no abacaxi
+                        E abaixa aqui pra mamar
+                        Bobsporra da calça gozada
+                        Fenda da sunga, nós tamo de volta
+                        Levando na bunda, quicando na água
+                        Sandy buceta tá querendo dá
+                        Ela não entende que nós só curte sentar`,
+    link : "https://www.youtube.com/watch?v=HIXTybbAgH8",
+    tags : "MC VV MCVV mcvv younglixo young lixo vito gemaplys",
+},
+
+    {titulo : "Espinossauro",
+    descricao : `Espinossauro (nome científico: Spinosaurus; cujo nome significa Lagarto Espinho) foi um gênero de dinossauro espinossaurídeo 
+            que viveu durante o Cenomaniano do período Cretáceo, entre 100 e 94 milhões de anos atrás, principalmente na região que é hoje o 
+            Norte da África[2]. Este gênero foi conhecido a partir de vestígios egípcios descobertos em 1912 e descritos em 1915 pelo paleontólogo 
+            Ernst Stromer. Esses vestígios originais foram destruídos na Segunda Guerra Mundial, mas material adicional foi encontrado no 
+            início do século XXI. O S. aegyptiacus é a espécie-tipo do gênero, embora tenhamos o S. maroccanus como uma espécie em potencial. 
+            Sobre seus sinônimos, o gênero Sigilmassasaurus já foi sinonimizado por alguns autores como um S. aegyptiacus, embora outros 
+            pesquisadores proponham que seja um gênero distinto, outro possível sinônimo é o Oxalaia da Formação Alcântara no Brasil, 
+            como um sinônimo não totalmente crescido de S. aegyptiacus.`,
+    link : "https://pt.wikipedia.org/wiki/Espinossauro",
+    tags : "Dinossauro espinossauro spinosaurus lagartoespinho bicho espinhão bicho espinhao bichoespinhao"    
+    },
+    
+];
+
+
+
+
+
